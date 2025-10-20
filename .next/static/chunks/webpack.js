6db580f45b9da1dde4f7191309e45b2b7beb8423
mxxx222 <mxxx222@users.noreply.github.com>
2025-10-20 06:41:58 +0300

    🎯 Complete Shopify multilingual template: FI/EN/RU homepage, navigation, footer - Ready for customer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "012c7c23910a404a"; }
+/******/ 		__webpack_require__.h = function() { return "10d36c6d5274c5fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
